36d1e0f74d42c2b764ac80bd1d5bf5dc478203cb
Brian Cap <briancap@pop-os.localdomain>
2022-08-25 21:43:39 -0400

    mysql settings, time zone, base migrations

--- hub/hub/settings.py
+++ hub/hub/settings.py
@@ -75,11 +75,12 @@ WSGI_APPLICATION = 'hub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Detroit'
 
 USE_I18N = True
 
